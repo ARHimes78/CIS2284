9e237b7029005ae65b66e5ad7d6777f399b7d92d
ARHimes78 <splabbity@Alans-MacBook-Pro.local>
2015-11-13 12:59:45 -0700

    new demos 111315
    new demos 111315 (description)

---
+++ class examples/MVCDemo/MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.MyReallyCoolKey = "My really cool value!";
+            return View();
+        }
+
+        public ActionResult PassingAModel()
+        {
+            var Contacts = new List<Contact>
+            {
+                new Contact {ContactID=1, Name="Bjarney Stroutrup" ,
+                Email = "bjarney@aserver.net", Phone="555-1212" },
+                new Contact {ContactID=2, Name="Ada Lovelace",
+                Email = "ada@aserver.net", Phone="555-1213"},
+                new Contact {ContactID=3, Name="Alan Turing",
+                Email="alan@aserver.net", Phone="555-1214"}
+            };
+
+            return View(Contacts);
+        }
+    }
+}
